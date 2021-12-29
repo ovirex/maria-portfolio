1a056ad3c53aef59a805c45ca309942c6630a230
Ovidio Perez <oapm10@gmail.com>
2021-12-29 02:52:08 -0400

    Delete unnecessary projects info

--- js/components/detail-components/description.js
+++ js/components/detail-components/description.js
@@ -13,14 +13,6 @@ export default {
         <p class="text-muted">{{desc}}</p>
         <div class="p-4 bg-light mb-5">
             <ul class="list-unstyled mb-0">
-                <li class="d-flex mb-2">
-                    <h6 class="mb-0">Category:</h6>
-                    <p class="ml-3 text-muted mb-0">Creative</p>
-                </li>
-                <li class="d-flex mb-2">
-                    <h6 class="mb-0">Date:</h6>
-                    <p class="ml-3 text-muted mb-0">23 Dec 2019</p>
-                </li>
                 <li class="d-flex">
                     <h6 class="mb-0">Tags:</h6>
                     <p class="ml-3 text-muted mb-0">
