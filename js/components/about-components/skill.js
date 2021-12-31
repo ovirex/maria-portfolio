0620d033ca05e5e04ead085e620a2bd847f44180
Ovidio Perez <oapm10@gmail.com>
2021-12-30 21:08:02 -0400

    Make the components use props for its info

--- js/components/about-components/skill.js
+++ js/components/about-components/skill.js
@@ -1,16 +1,26 @@
 export default {
+    props: {
+        skill: {
+            type: String,
+        },
+        desc: {
+            type: String,
+        },
+        svg_icon: {
+            type: String,
+        },
+    },
     template: `
     <div class="col-lg-6 mb-4">
         <div class="d-flex">
             <div class="flex-grow-1">
                 <svg class="svg-icon text-primary">
-                    <use xlink:href="#stack-1"> </use>
+                    <use v-bind:xlink:href="svg_icon"> </use>
                 </svg>
             </div>
             <div class="ml-3">
-                <h3 class="h5 mr-3">Graphic Designer</h3>
-                <p class="text-muted mb-2">eceived overcame oh sensible so at an. Formed do change merely to county it.
-                    Am separate contempt domestic to to oh. On relation my so addition branched.</p>
+                <h3 class="h5 mr-3">{{skill}}</h3>
+                <p class="text-muted mb-2">{{desc}}</p>
             </div>
         </div>
     </div>
